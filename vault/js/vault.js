a95b65f8c1e3ec41c7c0d91b66baf2bc1b52f186
aljadidsec <35167399+aljadidsec@users.noreply.github.com>
2024-01-31 12:43:51 -0600

    Update vault.js

--- vault/js/vault.js
+++ vault/js/vault.js
@@ -2,7 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         vaultNumber: null,
-        apiUrl: 'https://app.cloudispatcher.com/api/vaults/verify/',
+        apiUrl: 'https://clx.frb.io/api/vaults/verify/',
         profile: [],
         vault: [],
         address: [],
